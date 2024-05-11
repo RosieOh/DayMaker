ccee56197df52fc54a8765df88b20f52b6e41ffb
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-11 19:02:41 +0900

    ✨BUILD : DayMaker Spring Boot3.2.5 Init Setting #1

---
+++ src/main/java/com/daymaker/DaymakerApplication.java
@@ -0,0 +1,13 @@
+package com.daymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaymakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaymakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daymaker/DaymakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daymaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaymakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
